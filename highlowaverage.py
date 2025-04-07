9491cc19f6eb3612bebfe7df936b82e1c6b49b73
Julia Doyle <Doylej7457@clarkstate.edu>
2025-04-06 21:17:00 -0400

    upload

--- highlowaverage.py
+++ highlowaverage.py
@@ -7,3 +7,30 @@
 #Highest number
 #Lowest number
 #Correct answers for the included file:
+
+
+numberfile = open("numbers.txt", 'r')
+
+count = 0
+sum = 0
+max = 0
+min = 0
+
+for curline in numberfile:
+    num = int(curline)
+    count += 1
+    sum += num
+    if count == 1:
+        max = num
+        min = num
+    else:
+        if num > max:
+            max = num
+        if num < min:
+            min = num
+numberfile.close()
+print("The total of numbers is", count) 
+print("The sum is: ", sum)
+print("The average is: ", sum/count)
+print("The highest number is: ", max)
+print("The lowest number is: ", min)
